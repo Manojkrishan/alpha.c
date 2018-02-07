32d166e1439f54956e6d7c644afefcfbd02089dc
Manojkrishan <35995028+Manojkrishan@users.noreply.github.com>
2018-02-07 16:20:35 +0530

    largeno7.c

---
+++ largeno7.2.18.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+int a,b,c;
+printf("enter the value of a:");
+ scanf("%d",&a);
+printf("enter the value of b:");
+scanf("%d",&b);
+printf("enter the value of c:");
+scanf("%d",&c);
+if(a>b&&a>c)
+{
+printf("the number %d is greatest among 3 numbers",a);
+}
+else if(b>a&&b>c)
+{
+printf("the number %d is greatest among 3 numbers",b);
+}
+else 
+printf("the number %d is greatest among 3 numbers",c);
+}
+return 0;
+}
+
+
