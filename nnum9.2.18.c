ee5341ae774711f2d567f43ff68f957e3a80c06f
Manojkrishan <35995028+Manojkrishan@users.noreply.github.com>
2018-02-09 13:59:09 +0530

    nnum.9217.c

---
+++ nnum9.2.18.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+int a,i=0,b=0;
+printf("Enter the any number \n");
+scanf("%d",&a);
+for(i=0;i<=a;i=i+1)
+{
+  b=b+i;
+}
+printf("\n The sum nature number=%d",&a);
+retun )
+0;
+}
